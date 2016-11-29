0a16f76a1082be6910e6514cb9c7f919a8cee68c
ShevchenkoPavel <shevchenkopavel17@gmail.com>
2016-11-29 16:43:43 +0600

    bug-fixes, nunjucks, description-keyws-favicon, normalize-css, mini-css, autoprefixer

--- 4/gulpfile.js
+++ 4/gulpfile.js
@@ -1,29 +1,43 @@
 var gulp = require('gulp');
 var nunjucks = require('gulp-nunjucks');
 var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 
 var path = {
 	images: 'src/images/*',
     css:  'src/styles/*.css',
+	vendor: {
+	  css: 'src/vendor/css/*.css'
+	},
     html: 'src/templates/*.html',
     dist: {
 	  images: 'dist/images/',
       css:  'dist/styles/',
+	  vendor: 'dist/vendor/css/',
       html: 'dist/'
     }
 };
 
 gulp.task('default', ['build', 'serve', 'watch']);
 
-gulp.task('images', function () {
-  return gulp.src(path.images)
-    .pipe(gulp.dest(path.dist.images));
-});
-
 gulp.task('css', function () {
   return gulp.src(path.css)
+    .pipe(autoprefixer({
+        browsers: ['last 4 versions']
+    }))
     .pipe(concat('style.css'))
+    .pipe(gulp.dest(path.dist.css));
+});
+
+gulp.task('css-min', function () {
+  return gulp.src(path.css)
+    .pipe(autoprefixer({
+        browsers: ['last 4 versions']
+    }))
+    .pipe(concat('style.css'))
+    .pipe(cssmin())
     .pipe(gulp.dest(path.dist.css));
 });
 
@@ -33,12 +47,32 @@ gulp.task('html', function () {
     .pipe(gulp.dest(path.dist.html));
 });
 
-gulp.task('build', ['images', 'html', 'css']);
+gulp.task('images', function () {
+  return gulp.src(path.images)
+    .pipe(gulp.dest(path.dist.images));
+});
+
+gulp.task('vendor-css', function () {
+  return gulp.src(path.vendor.css)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest(path.dist.vendor));
+});
+
+gulp.task('vendor-css-min', function () {
+  return gulp.src(path.vendor.css)
+    .pipe(concat('vendor.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest(path.dist.vendor));
+});
+
+gulp.task('build', ['html', 'css', 'vendor-css', 'images']);
+gulp.task('prod', ['html', 'css-min', 'vendor-css-min', 'images']);
 
 gulp.task('watch', function () {
-  gulp.watch(path.images, ['images']);
   gulp.watch(path.css, ['css']);
   gulp.watch(path.html, ['html']);
+  gulp.watch(path.vendor.css, ['vendor-css']);
+  gulp.watch(path.img, ['images']);
 });
 
 gulp.task('serve', ['watch'], function() {
