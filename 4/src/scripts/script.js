aa46d1bb2780b9ab8db4172422102b4536536d24
ShevchenkoPavel <shevchenkopavel17@gmail.com>
2017-01-19 18:18:09 +0600

    fix 'read-more' button

--- 4/src/scripts/script.js
+++ 4/src/scripts/script.js
@@ -19,40 +19,6 @@ $(document).ready(function () {
 });
 
 
-$(document).ready(function(){
-  var pattern = /^[a-z0-9_-]+@[a-z0-9-]+\.[a-z]{2,6}$/i;
-  var mail = $('#mail');
-	
-	mail.blur(function(){
-		if(mail.val() != ''){
-				if(mail.val().search(pattern) == 0){
-					$('#valid').text('Подходит');
-					$('#submit').attr('disabled', false);
-					mail.removeClass('error').addClass('ok');
-				}else{
-					$('#valid').text('Не подходит');
-					$('#submit').attr('disabled', true);
-					mail.addClass('ok');
-				}
-			}else{
-				$('#valid').text('Поле e-mail не должно быть пустым!');
-				mail.addClass('error');
-				$('#submit').attr('disabled', true);
-			}
-	});
-    
-    
-    
-  $( function() {
-  $( "#datepicker" ).datepicker();
-  } );
-	
-
-	  Inputmask({"mask": "+7 (999) 999-9999"}).mask("#phone");
-  	
-    
-});
-
 
 $(document).ready(function(){
   var pattern = /^[a-zA-Z]+$/i;
@@ -99,4 +65,35 @@ $(document).ready(function(){
 				$('#submit').attr('disabled', true);
 			}
 	});
+});
+
+
+$(document).ready(function(){
+  var pattern = /^[a-z0-9_-]+@[a-z0-9-]+\.[a-z]{2,6}$/i;
+  var mail = $('#mail');
+	
+	mail.blur(function(){
+		if(mail.val() != ''){
+				if(mail.val().search(pattern) == 0){
+					$('#valid').text('Подходит');
+					$('#submit').attr('disabled', false);
+					mail.removeClass('error').addClass('ok');
+				}else{
+					$('#valid').text('Не подходит');
+					$('#submit').attr('disabled', true);
+					mail.addClass('ok');
+				}
+			}else{
+				$('#valid').text('Поле e-mail не должно быть пустым!');
+				mail.addClass('error');
+				$('#submit').attr('disabled', true);
+			}
+	});
+    
+  $( function() {
+  $( "#datepicker" ).datepicker();
+  } );
+	
+	  Inputmask({"mask": "+7 (999) 999-9999"}).mask("#phone");
+  	 
 });
